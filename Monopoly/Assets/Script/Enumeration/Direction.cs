d9d2060530eb8d1198abd896b735ab5044c261b3
frank chen <frank08.chen@gmail.com>
2018-11-29 21:02:23 +0800

    init class

---
+++ Monopoly/Assets/Script/Enumeration/Direction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public enum Direction
+{
+    North,
+    South,
+    East,
+    West
+}
---
+++ Monopoly/Assets/Script/Enumeration/Walkable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public enum Walkable
+{
+    Ape,
+    Human,
+    Both,
+    NoMan
+}
