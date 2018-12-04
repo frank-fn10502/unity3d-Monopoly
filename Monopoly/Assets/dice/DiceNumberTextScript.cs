f68a98d844201a99bc55cd3cf3f2648962051544
Davis <ho861206@gmail.com>
2018-12-04 14:40:26 +0800

    dice v1.0.0

--- Monopoly/Assets/dice/DiceNumberTextScript.cs
+++ Monopoly/Assets/dice/DiceNumberTextScript.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DiceNumberTextScript : MonoBehaviour {
+public class DiceNumberTextScript : MonoBehaviour
+{
 
 	Text text;
-	public static int diceNumber;
-
-	// Use this for initialization
-	void Start () {
+	public static int dice1Number;
+    public static int dice2Number;
+    public static int dice3Number;
+    // Use this for initialization
+    void Start ()
+    {
 		text = GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		text.text = diceNumber.ToString ();
+	void Update ()
+    {
+		text.text = (dice1Number + dice2Number + dice3Number).ToString ();
 	}
 }
--- Monopoly/Assets/dice/dice2/Dice2Script.cs
+++ Monopoly/Assets/dice/dice2/Dice2Script.cs
@@ -2,26 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiceScript : MonoBehaviour {
+public class Dice2Script : MonoBehaviour
+{
 
 	static Rigidbody rb;
-	public static Vector3 diceVelocity;
+	public static Vector3 dice2Velocity;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		diceVelocity = rb.velocity;
+	void Update ()
+    {
+		dice2Velocity = rb.velocity;
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			DiceNumberTextScript.diceNumber = 0;
+		if (Input.GetKeyDown (KeyCode.Space))
+        {
+			DiceNumberTextScript.dice2Number = 0;
 			float dirX = Random.Range (0, 500);
 			float dirY = Random.Range (0, 500);
 			float dirZ = Random.Range (0, 500);
-			transform.position = new Vector3 (0, 2, -75);
+			//transform.position = new Vector3 (0, 2, -2);
 			transform.rotation = Quaternion.identity;
 			rb.AddForce (transform.up * 500);
 			rb.AddTorque (dirX, dirY, dirZ);
