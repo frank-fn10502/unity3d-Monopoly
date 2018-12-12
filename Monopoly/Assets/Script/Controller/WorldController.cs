c68d7f0be46a69f27646843751ccd9ec19e46f4e
frank chen <frank08.chen@gmail.com>
2018-12-13 05:35:00 +0800

    display all game ui on board for player mode

--- Monopoly/Assets/Script/Controller/WorldController.cs
+++ Monopoly/Assets/Script/Controller/WorldController.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-//public delegate void PlayerAction();
-
 public class WorldController : MonoBehaviour
 {
     public World world;
-    public Camera characterCamera;
+    //public Camera characterCamera;
     void Start()
     {
         world = new World();
+        //gameObject.transform.position = world.CurrentGroup.Location + new Vector3(0 ,4 ,-4);
     }
     void Update()
     {
         world.execute();
-        characterCamera.transform.position = world.CurrentGroup.Location + new Vector3(0 ,4 ,-4);
+        gameObject.transform.position = world.CurrentGroup.Location + new Vector3(5 ,13 ,-6);
     }
 }
