9c509ff26bf3bad57bab06e4bfada3f66e472c8b
tigerlittle1 <tigerlittle1@gmail.com>
2018-12-27 00:39:42 +0800

    Event

---
+++ Monopoly/Assets/Script/Event/Personal_Event/Personal_EventPool.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personal_EventPool
+{
+    private int total_weight;
+    private List<EventBase> event_list;
+    private System.Random rand;
+
+    public Personal_EventPool()
+    {
+        total_weight = 0;
+        event_list = new List<EventBase>();
+        rand = new System.Random(DateTime.Now.Millisecond);
+    }
+
+    public void AddEvent(EventBase e)
+    {
+        total_weight += e.Weight;
+        event_list.Add(e);
+    }
+
+
+    public void RemoveEvent(int n)
+    {
+        if (n < event_list.Count)
+
+            total_weight -= event_list[n].Weight;
+        event_list.RemoveAt(n);
+    }
+
+    public void Clear()
+    {
+        total_weight = 0;
+        event_list.Clear();
+    }
+    public EventBase GetEvent()
+    {
+        int r = rand.Next(total_weight);
+        int w = 0;
+
+        EventBase e = event_list[0];
+
+        for (int i = 1; i < event_list.Count; ++i)
+        {
+            w += event_list[i - 1].Weight;
+            if (w <= r)
+            {
+                e = event_list[i];
+            }
+            else
+            {
+                break;
+            }
+
+        }
+        return e;
+    }
+}
