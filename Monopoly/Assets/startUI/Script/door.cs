0f28b493eb7fbad3094e37eeca5c7c77b6faa1d0
Davis <ho861206@gmail.com>
2018-12-28 23:04:31 +0800

    Add StartScene

---
+++ Monopoly/Assets/startUI/Script/door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour {
+
+    // Use this for initialization
+    public void OnStartGame(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
