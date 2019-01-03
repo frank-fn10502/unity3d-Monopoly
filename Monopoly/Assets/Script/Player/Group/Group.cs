5110d05af8c213c3c2289c82d3e7bc0278f9313f
frank chen <frank08.chen@gmail.com>
2019-01-03 22:05:52 +0800

    some edit

--- Monopoly/Assets/Script/Player/Group/Group.cs
+++ Monopoly/Assets/Script/Player/Group/Group.cs
@@ -2,7 +2,7 @@
 
 public class Group
 {
-    private Block[] blockList;
+    public static Block[] blockList;
     private PlayerState state;
     private Walkable    identity;
     private Skill skill;
@@ -109,17 +109,9 @@ public class Group
             inJailTime = value;
         }
     }
-    public Block[] BlockList
-    {
-        get
-        {
-            return blockList;
-        }
-    }
 
-    public Group(Block[] blockList , Skill skill ,Actor[] actors ,Attributes attributes ,Resource resource ,Vector3 location ,int currentBlockIndex ,Direction enterDirection)
+    public Group(Skill skill ,Actor[] actors ,Attributes attributes ,Resource resource ,Vector3 location ,int currentBlockIndex ,Direction enterDirection)
     {
-        this.blockList = blockList;
         this.state = PlayerState.Normal;
         this.identity = Walkable.Human;
         this.skill = skill;
