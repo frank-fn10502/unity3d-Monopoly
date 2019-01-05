de67b34deb1e05c41a6ea3f9813f00cb6d33ac1c
frank chen <frank08.chen@gmail.com>
2019-01-05 17:00:26 +0800

    can hide playerMsg

---
+++ Monopoly/Assets/Script/Controller/PlayerSideMsgController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSideMsgController : MonoBehaviour
+{
+    public void showButtonClick()
+    {
+        gameObject.SetActive(true);
+    }
+    public void hideButtonClick()
+    {
+        gameObject.SetActive(false);
+    }
+}
