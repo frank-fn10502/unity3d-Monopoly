9e4cdff764197d7c5b6c23057e997076a9cf7cd8
frank chen <frank08.chen@gmail.com>
2019-01-07 03:35:58 +0800

    dice can work

--- Monopoly/Assets/dice/dice1/Dice1Script.cs
+++ Monopoly/Assets/dice/dice1/Dice1Script.cs
@@ -7,12 +7,15 @@ public class Dice1Script : MonoBehaviour
 
 	static Rigidbody rb;
 	public static Vector3 dice1Velocity;
-
-	// Use this for initialization
-	void Start ()
+    private Quaternion quate;
+    
+    // Use this for initialization
+    void Start ()
     {
 		rb = GetComponent<Rigidbody> ();
-	}
+        quate = Quaternion.identity;
+        quate.eulerAngles = new Vector3(0 ,0 ,0);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -21,12 +24,15 @@ public class Dice1Script : MonoBehaviour
 
 		if (Input.GetKeyDown (KeyCode.Space))
         {
-			DiceNumberTextScript.dice1Number = 0;
-			float dirX = Random.Range (0, 500);
+            gameObject.transform.position += new Vector3(0 ,2f ,0);
+            gameObject.transform.rotation = quate;
+
+            //DiceNumberTextScript.dice1Number = 0;
+            float dirX = Random.Range (0, 500);
 			float dirY = Random.Range (0, 500);
 			float dirZ = Random.Range (0, 500);
-			//transform.position = new Vector3 (-392 ,82,-12);
-			transform.rotation = Quaternion.identity;
+
+
 			rb.AddForce (transform.up * 1000);
 			rb.AddTorque (dirX, dirY, dirZ);
 		}
