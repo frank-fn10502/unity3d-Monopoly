c1d209ef5f0626b8a3136ca512904707f6d15db6
keivn1114 <asd3136396@gmail.com>
2019-01-07 05:56:29 +0800

    ShowEventController add

---
+++ Monopoly/Assets/Script/Controller/ShowEventController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowEventController : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject obj;
+    string[] vs;
+
+    void Start ()
+    {
+        obj = GameObject.Find("WorldMsgShow/TheWorldMsg");
+        string str = obj.GetComponent<Control>().GetText();
+        vs = str.Split('\n');
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach (string s in vs)
+        {
+            GameObject.Find("EndMsgShow/TheEndMsg").GetComponent<Control>().WriteText(s);
+        }
+	}
+}
