14298cd238d37cc08c8e4a9f71a37eac30951f9a
tigerlittle1 <tigerlittle1@gmail.com>
2019-01-07 05:06:14 +0800

    no message

--- Monopoly/Assets/Script/Event/Word_Event/EarthquakeEvent.cs
+++ Monopoly/Assets/Script/Event/Word_Event/EarthquakeEvent.cs
@@ -13,7 +13,7 @@ class EarthquakeEvent : EventBase
         public override void DoEvent(List<Group> droup_list, Group group)
         {
             //發生地震，所有玩家損失10%人口
-            for(int i = 0;i< droup_list.Count+1;i++)
+            for(int i = 0;i< droup_list.Count;i++)
             {
                 droup_list[i].Resource.civilian = Convert.ToInt32(droup_list[i].Resource.civilian*0.9);
             }
